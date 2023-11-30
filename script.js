325591113f51140323a62f25a028ce5794bc9eef
Tyler Brown <brown.tyler.w@gmail.com>
2023-11-30 11:09:30 -0800

    Added home screen and home button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function load(id) {
-    ids = ['home', 'bad', 'hurt', 'proof'];
-    for (let i = 0; i < 4; i++) {
+    // Hide everything except for linked element
+    ids = ['home', 'what', 'bad', 'hurt', 'proof', 'action'];
+    for (let i = 0; i < 6; i++) {
         if (ids[i] != id) {
             const hide = document.getElementById(ids[i]);
             hide.className = "hidden";
