dd1e66dc24a0804876a3d5f0e0ca277adcac38a2
Tyler Brown <brown.tyler.w@gmail.com>
2023-11-30 11:47:15 -0800

    Style and JS finished Section content will now load 1.5 seconds after link to section is clicked

--- script.js
+++ script.js
@@ -4,9 +4,25 @@ function load(id) {
     for (let i = 0; i < 6; i++) {
         if (ids[i] != id) {
             const hide = document.getElementById(ids[i]);
+            const hideinfo = document.getElementById(ids[i]+"info");
             hide.className = "hidden";
+            hideinfo.className = "hidden";
         }
     }
     const link = document.getElementById(id);
     link.className = "box";
+    setTimeout(loadinfo, 1500, id+"info");
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function loadinfo(id) {
+    const content = document.getElementById(id);
+    content.className = 'info';
+}
+
+window.onload = function() {
+    setTimeout(loadinfo, 1500, 'homeinfo');
 }
